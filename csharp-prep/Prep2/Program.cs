84cf03568665e747e334eaf5b994027417fb09be
Christopher Ahlstrom <christopherahlstrom98@gmail.com>
2026-04-24 15:17:32 -0700

    I replaced the code in Prep1/Programs.cs and Prep2/Program.cs with the code for the respective assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,93 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        float gradePercent = float.Parse(userInput);
+
+        if (gradePercent >= 90.0)
+        {
+            if (gradePercent >= 97)
+            {
+                Console.WriteLine("You got an A+!");
+            }
+            else if (gradePercent % 10 <= 3)
+            {
+                Console.WriteLine("You got an A-!");
+            }
+            else
+            {
+                Console.WriteLine("You got an A!");
+            }
+        }
+        else if (gradePercent >= 80.0)
+        {
+            if (gradePercent % 10 >= 7)
+            {
+                Console.WriteLine("You got an B+.");
+            }
+            else if (gradePercent % 10 <= 3)
+            {
+                Console.WriteLine("You got an B-.");
+            }
+            else
+            {
+                Console.WriteLine("You got an B.");
+            }
+        }
+        else if (gradePercent >= 70.0)
+        {
+            if (gradePercent % 10 >= 7)
+            {
+                Console.WriteLine("You got an C+.");
+            }
+            else if (gradePercent % 10 <= 3)
+            {
+                Console.WriteLine("You got an C-.");
+            }
+            else
+            {
+                Console.WriteLine("You got an C.");
+            }
+        }
+        else if (gradePercent >= 60.0)
+        {
+            if (gradePercent % 10 >= 7)
+            {
+                Console.WriteLine("You got an D+.");
+            }
+            else if (gradePercent % 10 <= 3)
+            {
+                Console.WriteLine("You got an D-.");
+            }
+            else
+            {
+                Console.WriteLine("You got an D.");
+            }
+        }
+        else 
+        {
+            if (gradePercent % 10 >= 7)
+            {
+                Console.WriteLine("You got an F+.");
+            }
+            else if (gradePercent <= 53)
+            {
+                Console.WriteLine("You got an F-.");
+            }
+            else
+            {
+                Console.WriteLine("You got an F.");
+            }
+        }
+
+        if(gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you failed the class.");
+        }
     }
 }
