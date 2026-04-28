909cd49649d0370c91b610495f607ee03250c0b8
Christopher Ahlstrom <christopherahlstrom98@gmail.com>
2026-04-28 14:39:26 -0700

    I added code to meet the stretch goals.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 100);
-        int guess;
+        string playAgain;
         do
         {
-            Console.Write("What is your guess? ");
-            string userInput = Console.ReadLine();
-            guess = int.Parse(userInput);
-            if (number < guess)
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            int guess;
+            int numOfGuesses = 0;
+            do
             {
-                Console.WriteLine("Lower");
-            }
-            else if (number > guess)
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                numOfGuesses++;
+            } while (guess != number);
+            if (numOfGuesses == 1)
             {
-                Console.WriteLine("Higher");
+                Console.WriteLine($"You guessed the number in {numOfGuesses} guess!");
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed the number in {numOfGuesses} guesses!");
             }
-        } while (guess != number);
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+        
     }
 }
