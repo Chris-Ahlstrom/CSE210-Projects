db7f3f57c6077ad3abafeb551ab97a376223272c
Christopher Ahlstrom <christopherahlstrom98@gmail.com>
2026-04-28 14:22:11 -0700

    I replaced the code with code for the assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
     }
 }
