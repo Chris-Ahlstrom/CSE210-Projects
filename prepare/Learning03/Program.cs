9e0192a109b87be9a910bff299c076a8d6e67fc4
Christopher Ahlstrom <christopherahlstrom98@gmail.com>
2026-05-19 12:07:12 -0700

    I wrote the code for the Fraction prepare project.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Random rnd = new Random();
+        for (int i = 1; i < 21; i++)
+        {
+            one.SetTop(rnd.Next(0, 20));
+            one.SetBottom(rnd.Next(0, 20));
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"Fraction {i}: String: {one.GetFractionString()} Number: {one.GetDecimalValue()}");
+        } 
     }
 }
